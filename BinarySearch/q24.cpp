4ccfa94a0389c34c059132af838d13b7df111cf3
Siddhant Shah <siddhantshah469@gmail.com>
2025-02-16 15:14:40 +0530

    Add new file for binary search question 24 implementation

---
+++ BinarySearch/q24.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int> &boards, int k, int mid) 
+{
+    int painterCount = 1;
+    int timeSum = 0;
+    
+    for(int i = 0; i<boards.size(); i++) 
+    {
+        if(timeSum + boards[i] <= mid) 
+        {
+            timeSum += boards[i];
+        }
+        else
+        {
+            painterCount++;
+            if(painterCount > k || boards[i] > mid ) 
+            {
+              return false;
+            }
+            timeSum = boards[i];
+        }
+    }
+    return true;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    int s = 0;
+    int sum = 0;
+    
+    for(int i = 0; i<boards.size(); i++) 
+    {
+        sum += boards[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e)
+    {
+        if(isPossible(boards,k,mid)) 
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
