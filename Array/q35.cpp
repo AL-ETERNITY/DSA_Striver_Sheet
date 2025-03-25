3e2a780e0d12b71d58737883342a8a99185f53b4
Siddhant Shah <siddhantshah469@gmail.com>
2025-03-25 23:10:01 +0530

    Add new file for binary search question 35 implementation

---
+++ Array/q35.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int rows = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<rows;i++){
+            vector<int>& last = ans.back(); 
+            if (last[1] >= intervals[i][0]) {
+                last[1] = max(last[1], intervals[i][1]);
+            } 
+            else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
