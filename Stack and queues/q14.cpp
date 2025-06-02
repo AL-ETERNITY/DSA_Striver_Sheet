ffebfca38c2cdb5f50ce5b52e3c2e3a0c52fa8eb
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-02 15:48:54 +0530

    Add implementation of prefix to postfix conversion using stack

---
+++ Stack and queues/q14.cpp
@@ -0,0 +1,28 @@
+// implementation of prefix to postfix conversion using stack
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+  public:
+    string preToPost(string pre_exp) {
+        int n = pre_exp.length();
+        int i = n-1;
+        stack<string> st;
+       while(i>=0){
+           if((pre_exp[i]>='A' && pre_exp[i]<='Z') || (pre_exp[i]>='a' && pre_exp[i]<='z') || (pre_exp[i]>='0' && pre_exp[i]<='9')){
+               st.push(string{pre_exp[i]});
+            }
+           else{
+                string t1 = st.top(); st.pop();
+                string t2 = st.top(); st.pop();
+                   
+                string con_str = t1 + t2 + pre_exp[i];
+                st.push(con_str);
+            }
+           i--;
+       }
+       return st.top();
+    }
+};
