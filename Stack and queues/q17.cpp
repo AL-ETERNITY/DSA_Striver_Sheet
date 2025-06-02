7f8d9a2eeb4c5656d9b97ffcbfa542d99822b1b8
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-02 19:25:59 +0530

    Add implementation of next smaller element using stack

---
+++ Stack and queues/q17.cpp
@@ -0,0 +1,22 @@
+// implementing next smaller element using stack
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> prevSmaller(vector<int> &A) {
+    vector<int> G;
+    stack<int> st;
+    for (int i = 0; i < A.size(); i++) {
+        while (!st.empty() && st.top() >= A[i]) {
+            st.pop();
+        }
+        if (st.empty()) {
+            G.push_back(-1);
+        } else {
+            G.push_back(st.top());
+        }
+        st.push(A[i]);
+    }
+    return G;
+}
