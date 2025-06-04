09ffedc49171c5329fcbea3e7b2e44653ce8473a
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-04 17:54:24 +0530

    Add implementation of maximal rectangle in a binary matrix problem using stack

---
+++ Stack and queues/q25.cpp
@@ -0,0 +1,79 @@
+// implemented maximal rectangle in a binary matrix problem using stack
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    vector<int> NSE(vector<int> arr){
+        int n = arr.size();
+        stack<int> st;
+        vector<int> nse(n);
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(st.empty()) nse[i] = n;
+            else nse[i] = st.top();
+
+            st.push(i); 
+        }
+        return nse;
+    }
+
+    vector<int> PSE(vector<int> arr){
+        int n = arr.size();
+        stack<int> st;
+        vector<int> pse(n);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(st.empty()) pse[i] = -1;
+            else pse[i] = st.top();
+
+            st.push(i);
+        }
+        return pse;    
+    }
+
+    int largestRectangleArea(vector<int> heights) {
+        int n = heights.size();
+        vector<int> next = NSE(heights);
+        vector<int> prev = PSE(heights);
+
+        int area = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            int l = heights[i];
+            int b = next[i] - prev[i] - 1;
+            int newArea = l * b;
+            area = max(area, newArea);
+        }
+        return area;
+    }
+
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.empty() || matrix[0].empty()) return 0;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> heights(rows, vector<int>(cols, 0));
+        int maxArea = 0;
+
+        for(int j = 0; j < cols; j++){
+            int psum = 0;
+            for(int i = 0; i < rows; i++){
+                if(matrix[i][j] == '1') psum += 1;
+                else psum = 0;
+                heights[i][j] = psum;
+            }
+        }
+
+        for(int i = 0; i < rows; i++){
+            maxArea = max(maxArea, largestRectangleArea(heights[i]));
+        }
+
+        return maxArea;
+    }
+};
