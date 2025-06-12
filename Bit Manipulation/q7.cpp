400364fdc931726c4fd5b08a61717712993bec6d
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-12 19:06:23 +0530

    Add implementation of function to swap two numbers using bit manipulation

---
+++ Bit Manipulation/q7.cpp
@@ -0,0 +1,14 @@
+// function to swap two numbers using bit manipulation
+#include <iostream> 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    pair<int, int> get(int a, int b) {
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        return {a,b};
+    }
+};
