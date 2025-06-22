39f419f4bc5b0d276d2f20f7745121d3cc58e0a2
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-23 00:15:13 +0530

    Add implementation of Solution class to rotate a linked list to the right by k places

---
+++ Linked List/q29.cpp
@@ -0,0 +1,42 @@
+// function to solve the problem of rotating a linked list to the right by k places
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head -> next) return head;
+        
+        int length = 0;
+        ListNode* curr = head;
+        while(curr) {
+            length++;
+            curr = curr -> next;
+        }
+
+        k = k % length;
+        if(!k) return head;
+
+        curr = head;
+        ListNode* next = curr -> next;
+        for(int i = 0; i < (length - k - 1); i++) {
+            curr = curr -> next;
+            next = curr -> next;
+        }
+        curr -> next = nullptr;
+        
+        ListNode* nextCpy = next;
+        while(nextCpy -> next) {
+            nextCpy = nextCpy -> next;
+        }
+        nextCpy -> next = head;
+
+        return next;
+    }
+};
