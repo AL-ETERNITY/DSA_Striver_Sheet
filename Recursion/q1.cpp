5c4f6adf5f9c671c73744ace5aa6b32302e25c1c
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-23 18:06:39 +0530

    Refactor myAtoi implementation to use recursion for string to integer conversion

--- Recursion/q1.cpp
+++ Recursion/q1.cpp
@@ -1,30 +1,30 @@
-// function to convert a string to an integer (atoi)
-#include <iostream>
-#include <string>
-#include <climits>
+// function to solve the problem of converting a string to an integer
+#include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
+private:
+    int recur(string s, int i, int sign, long long res){
+        if(i >= s.size() || !isdigit(s[i])) return res*sign;
+
+        res = res*10 + s[i] - '0';
+
+        if(res*sign <= INT_MIN) return INT_MIN;
+        if(res*sign >= INT_MAX) return INT_MAX;
+
+        return recur(s, i+1, sign, res);
+    }
 public:
     int myAtoi(string s) {
-        int i = 0, sign = 1;
-        long res = 0;
+        size_t start = s.find_first_not_of(" ");
+        if(start == string::npos) return 0;
 
-        while (i < s.size() && s[i] == ' ') i++;
-        if (i == s.size()) return 0;
+        s = s.substr(start);
+        if(s.empty()) return 0;
 
-        if (s[i] == '-') { sign = -1; i++; }
-        else if (s[i] == '+') i++;
+        int sign = 1, index = 0;
+        if(s[0] == '-') sign = -1, index = 1;
+        else if(s[0] == '+') index = 1; 
 
-        while (i < s.size() && isdigit(s[i])) {
-            res = res * 10 + (s[i] - '0');
-
-            if (sign * res > INT_MAX) return INT_MAX;
-            if (sign * res < INT_MIN) return INT_MIN;
-
-            i++;
-        }
-
-        return (int)(sign * res);
+        return recur(s, index, sign, 0);
     }
 };
