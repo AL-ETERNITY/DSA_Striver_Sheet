7a4e7c15a77c21426465295216c4a25005691399
Siddhant Shah <siddhantshah469@gmail.com>
2025-06-30 05:24:03 +0530

    Add multiple implementations of lengthOfLIS using different dynamic programming approaches

--- dynamic programming/q39.cpp
+++ dynamic programming/q39.cpp
@@ -23,3 +23,62 @@ public:
         return f(nums, 0, -1, n, dp);
     }
 };
+
+// bottom-up dynamic programming approach
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        for(int index = n-1; index >= 0; index--){
+            for(int prevIndex = index-1; prevIndex >= -1; prevIndex--){
+                int len = 0 + dp[index+1][prevIndex+1];
+                if(prevIndex == -1 || nums[index] > nums[prevIndex]){
+                    len = max(len, 1 + dp[index+1][index+1]);
+                }
+                dp[index][prevIndex+1] = len;
+            }
+        }
+        return dp[0][-1+1];
+    }
+};
+
+// space optimized bottom-up dynamic programming approach
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> next(n+1, 0), curr(n+1, 0);
+        for(int index = n-1; index >= 0; index--){
+            for(int prevIndex = index-1; prevIndex >= -1; prevIndex--){
+                int len = 0 + next[prevIndex+1];
+                if(prevIndex == -1 || nums[index] > nums[prevIndex]){
+                    len = max(len, 1 + next[index+1]);
+                }
+                curr[prevIndex+1] = len;
+            }
+            next = curr;
+        }
+        return next[-1+1];
+    }
+};
+
+// even more space optimized bottom-up dynamic programming approach
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);  // Initialize dp array with 1s
+        int maxLength = 1;  // At least one element is always an increasing subsequence
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if (nums[i] > nums[prev]) {
+                    dp[i] = max(dp[i], dp[prev] + 1);
+                }
+            }
+            maxLength = max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+};
