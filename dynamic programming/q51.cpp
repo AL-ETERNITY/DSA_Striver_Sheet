75bab08d426f70e540c8198d9946466f3f04247d
Siddhant Shah <siddhantshah469@gmail.com>
2025-07-02 04:58:15 +0530

    Add initial implementation of Solution class for maximum sum after partitioning an array problem

---
+++ dynamic programming/q51.cpp
@@ -0,0 +1,30 @@
+// function to solve the problem of maximum sum after partitioning an array
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int solve(int i, int n, vector<int> &arr, int k, vector<int> &dp) {
+        if(i >= n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int cnt = 0;
+        int maxi = arr[i];
+        int toReturn = INT_MIN;
+        for(int partition = i; partition < min(n, i + k); partition++) {
+            cnt++;
+
+            maxi = max(maxi, arr[partition]);
+            toReturn = max(toReturn, cnt * maxi + solve(partition + 1, n, arr, k, dp));
+        }
+
+        return dp[i] = toReturn;
+    }
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n + 1, -1);
+        return solve(0, n, arr, k, dp);
+    }
+};
