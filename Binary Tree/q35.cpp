1a6cb6242ae5553660d155428145d926e0b5121d
Siddhant Shah <siddhantshah469@gmail.com>
2025-07-13 01:45:54 +0530

    Add initial implementation of Morris traversal for inorder traversal of a binary tree

---
+++ Binary Tree/q35.cpp
@@ -0,0 +1,44 @@
+// function to solve inorder traversal of a binary tree using morris traversal
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+    TreeNode* find(TreeNode* root) {
+        TreeNode* pred = root->left;
+        while (pred->right != NULL && pred->right != root) {
+            pred = pred->right;
+        }
+        return pred;
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr = root;
+        vector<int> ans;
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* pred = find(curr);
+                if (pred->right == NULL) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
