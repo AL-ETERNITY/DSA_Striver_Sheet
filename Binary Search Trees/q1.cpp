4f2522003a7c2b84a911cd0cda3de357392bfc82
Siddhant Shah <siddhantshah469@gmail.com>
2025-07-27 02:42:22 +0530

    Add initial implementation of function to check if an array represents a valid BST traversal

---
+++ Binary Search Trees/q1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& arr) {
+        for(int i = 1; i < arr.size(); i++) {
+            if(arr[i] <= arr[i - 1]) return false;
+        }
+        return true;
+    }
+};
