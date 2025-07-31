0a187f2a188756d4ad454a44c8d5dad9c7cf00d7
Siddhant Shah <siddhantshah469@gmail.com>
2025-08-01 00:00:34 +0530

    Refactor check function to count inversions instead of rotating the array

--- Array/q3.cpp
+++ Array/q3.cpp
@@ -1,46 +1,29 @@
-#include <iostream>
-#include<vector>
 #include<bits/stdc++.h>
 using namespace std;
 
 
 class Solution {
 public:
-    void rotate(vector<int>& nums,int x){
-        vector<int> temp(nums.size());
-        for (int i=0;i<nums.size();i++)
-        {
-            temp[(i+x)%nums.size()]=nums[i];
-        }
-        nums=temp;
-    }
-    
-    
-    
     bool check(vector<int>& nums) {
-        int x=0;
-        int k=0;
-        while (k<=nums.size())
-        {
-            if (nums.size()==1)
-            {
-                return true;
-            }
-            rotate(nums,x);
-            for (int j=1;j<nums.size();j++)
-            {
-                if (nums[j-1]>nums[j])
-                {
-                  k++;
-                  x=1;
-                  break;
-                }
-                else if(j==nums.size()-1)
-                {
-                    return true;
-                }
+        int n = nums.size();
+        if (n <= 1) return true;
+
+        int inversionCount = 0;
+
+        // For every pair, count the number of inversions.
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                ++inversionCount;
+                if (inversionCount > 1) return false;
             }
         }
-        return false;
+
+        // Also check between the last and
+        // the first element due to rotation
+        if (nums[0] < nums[n - 1]) {
+            ++inversionCount;
+        }
+
+        return inversionCount <= 1;
     }
 };
